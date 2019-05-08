b056c9528540709f2e9010c45158f76445c7f670
Ayushi Hasija <ayushi.hasija@knoldus.in>
2019-05-08 13:27:34 +0530

    Working Code with Attendees

--- src/main/scala/InviteWithLatestVersion.scala
+++ src/main/scala/InviteWithLatestVersion.scala
@@ -29,15 +29,15 @@ class InviteWithLatestVersion {
 
   def send(): Unit = {
     try {
-
-      val from = "sakshi.hasija24@gmail.com"
+      val from = "hasijaayushi@gmail.com"
       val to = "ayushi.hasija@knoldus.in"
+      val toAttendee = "sakshi.gupta@knoldus.in"
       val prop = new Properties
       prop.put("mail.smtp.auth", "true")
       prop.put("mail.smtp.starttls.enable", "true")
       prop.put("mail.smtp.host", "smtp.gmail.com")
       prop.put("mail.smtp.port", "587")
-      val username = "hasijaayushi@gmail.com"
+      val username = from
       val password = "Shasija24"
       val session = Session.getInstance(prop, new Authenticator() {
         override protected def getPasswordAuthentication = new PasswordAuthentication(username, password)
@@ -50,6 +50,7 @@ class InviteWithLatestVersion {
       message.addHeaderLine("component=VEVENT")
       message.setFrom(new InternetAddress(from))
       message.addRecipient(Message.RecipientType.TO, new InternetAddress(to))
+      message.addRecipient(Message.RecipientType.TO, new InternetAddress(toAttendee))
       message.setSubject("******Calendar Invite***************")
 
       val calendar = new Calendar
@@ -59,31 +60,36 @@ class InviteWithLatestVersion {
       calendar.getProperties.add(Method.REQUEST)
       calendar.getProperties.add(new TzId("Asia/Kolkatta"))
 
-      val fromDate = new DateTime(1557234600000L)
+      val fromDate = new DateTime(1557302435000L)
       fromDate.setUtc(true)
-      val toDate = new DateTime(1557235200000L)
+      val toDate = new DateTime(1557303635000L)
       toDate.setUtc(true)
       val meeting = new VEvent(fromDate, toDate, "Booking Confirmation Invite")
 
-      val organizer = new Organizer("mailto:sakshi.hasija24@gmail.com")
+      val organizer = new Organizer(URI.create("mailto:" + "hasijaayushi@gmail.com"))
       meeting.getProperties.add(organizer)
       meeting.getProperties.add(new Uid(UUID.randomUUID().toString))
 
       val location = new Location("bravos")
       meeting.getProperties.add(location)
 
+      import net.fortuna.ical4j.model.property.Clazz
+      import net.fortuna.ical4j.model.property.Priority
+      meeting.getProperties.add(Priority.MEDIUM)
+      meeting.getProperties.add(Clazz.PUBLIC)
+
       import net.fortuna.ical4j.model.parameter.Cn
       import net.fortuna.ical4j.model.property.Attendee
       import java.net.URI
 
-      val dev1 = new Attendee("mailto:ayushi.hasija@knoldus.in")
+      val dev1 = new Attendee(URI.create("mailto:" +"ayushi.hasija@knoldus.in"))
       dev1.getParameters.add(PartStat.NEEDS_ACTION)
       dev1.getParameters.add(Rsvp.TRUE)
       dev1.getParameters.add(CuType.INDIVIDUAL)
       dev1.getParameters.add(new Cn("Developer 1"))
       meeting.getProperties.add(dev1)
 
-      val dev2 = new Attendee("mailto:hasijaayushi@gmail.com")
+      val dev2 = new Attendee(URI.create("mailto:" +"sakshi.gupta@knoldus.in"))
       dev2.getParameters.add(PartStat.NEEDS_ACTION)
       dev2.getParameters.add(Rsvp.TRUE)
       dev2.getParameters.add(CuType.INDIVIDUAL)
@@ -104,7 +110,11 @@ class InviteWithLatestVersion {
 
       println(calendar.getComponents)
       val messageBodyPart = new MimeBodyPart
-
+      /*messageBodyPart.setHeader("Content-Class", "urn:content-  classes:calendarmessage")
+      messageBodyPart.setHeader("Content-ID", "calendar_message")
+      messageBodyPart.setHeader("Content-Disposition", "inline")
+      */
+      messageBodyPart.setContent(calendar.toString, "text/calendar")
       messageBodyPart.setHeader("charset", "UTF-8")
       messageBodyPart.setHeader("Content-Transfer-Encoding","7bit")
       messageBodyPart.setDataHandler(new DataHandler(new ByteArrayDataSource(calendar.toString, "text/calendar;Content-Type= \"application/ics\";" +
@@ -115,7 +125,6 @@ class InviteWithLatestVersion {
       multipart.addBodyPart(messageBodyPart)
       message.setContent(multipart)
       Transport.send(message)
-      System.out.println("Success")
     } catch {
       case me: MessagingException =>
         me.printStackTrace()
